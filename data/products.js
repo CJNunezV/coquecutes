e836e485f2927b42cc4572fafddbcd78453f9453
christopher-airc <christopher@airc.digital>
2026-08-04 13:32:35 -0500

    aa

--- data/products.js
+++ data/products.js
@@ -7,7 +7,7 @@ export const products = [
     images: ["/team-rocket-1.jpg", "/team-rocket-2.jpg", "/team-rocket-3.jpg"],
     thumbnail: "/team-rocket-png.png",
     thumbnailOffsetY: 40, // px: negativo sube la imagen, positivo la baja
-    thumbnailScale: 1.1,
+    thumbnailScale: 1,
     description: "Protector premium diseñado especialmente para cartas coleccionables de formato estándar. Acabado negro y rojo con el icónico logo \"R\" del Team Rocket, para tus cartas con más actitud.",
     specs: {
       medidas: "Exteriores: 10.5 cm x 8.2 cm | Interiores (para la carta): 9.2 cm x 6.8 cm",
@@ -24,7 +24,7 @@ export const products = [
     images: ["/ultra-ball-1.jpg", "/ultra-ball-2.jpg", "/ultra-ball-3.jpg"],
     thumbnail: "/ultra-ball-png.png",
     thumbnailOffsetY: 45,
-    thumbnailScale: 1.1,
+    thumbnailScale: 1,
     description: "Inspirado en la Ultra Ball: acabado negro con banda amarilla y botón central blanco. Misma resistencia y precisión de encastre que el resto de la línea premium.",
     specs: {
       medidas: "Exteriores: 10.5 cm x 8.2 cm | Interiores (para la carta): 9.2 cm x 6.8 cm",
@@ -41,7 +41,7 @@ export const products = [
     images: ["/premier-ball-1.jpg", "/premier-ball-2.jpg", "/premier-ball-3.jpg"],
     thumbnail: "/premier-ball-png.png",
     thumbnailOffsetY: 40,
-    thumbnailScale: 1.1,
+    thumbnailScale: 1,
     description: "Inspirado en la Premier Ball: acabado blanco con franja roja central y botón blanco. Elegante y minimalista para tus cartas más valiosas.",
     specs: {
       medidas: "Exteriores: 10.5 cm x 8.2 cm | Interiores (para la carta): 9.2 cm x 6.8 cm",
@@ -58,7 +58,7 @@ export const products = [
     images: ["/pokebola-1.jpg", "/pokebola-2.jpg", "/pokebola-3.jpg"],
     thumbnail: "/pokebola-png.png",
     thumbnailOffsetY: 60,
-    thumbnailScale: 1.1,
+    thumbnailScale: 1,
     description: "Inspirado en el diseño icónico de la Poké Ball original: rojo arriba, blanco abajo, con línea negra divisoria. El clásico que nunca falla.",
     specs: {
       medidas: "Exteriores: 10.5 cm x 8.2 cm | Interiores (para la carta): 9.2 cm x 6.8 cm",

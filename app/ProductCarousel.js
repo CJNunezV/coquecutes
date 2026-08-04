06cd3ebfbd6b0c425cd2a5e79b9aee867f14d690
christopher-airc <christopher@airc.digital>
2026-08-04 00:42:21 -0500

    carr

--- app/ProductCarousel.js
+++ app/ProductCarousel.js
@@ -127,7 +127,7 @@ export default function ProductCarousel({ products }) {
                 }}
               >
                 <img
-                  src={(product.images && product.images[0]) || "/placeholder.svg"}
+                  src={product.thumbnail || (product.images && product.images[0]) || "/placeholder.svg"}
                   alt={product.name}
                   style={{ width: "60%", height: "60%", objectFit: "contain" }}
                 />

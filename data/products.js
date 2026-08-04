3ad53ef1096777c678405c53d1155badf66cbf28
christopher-airc <christopher@airc.digital>
2026-08-04 11:10:28 -0500

    a

--- data/products.js
+++ data/products.js
@@ -6,8 +6,8 @@ export const products = [
     price: 30.00,
     images: ["/team-rocket-1.jpg", "/team-rocket-2.jpg", "/team-rocket-3.jpg"],
     thumbnail: "/team-rocket-png.png",
-    thumbnailOffsetY: -10, // px: negativo sube la imagen, positivo la baja
-    thumbnailScale: 1.15,
+    thumbnailOffsetY: -50, // px: negativo sube la imagen, positivo la baja
+    thumbnailScale: 1.5,
     description: "Protector premium diseñado especialmente para cartas coleccionables de formato estándar. Acabado negro y rojo con el icónico logo \"R\" del Team Rocket, para tus cartas con más actitud.",
     specs: {
       medidas: "Exteriores: 10.5 cm x 8.2 cm | Interiores (para la carta): 9.2 cm x 6.8 cm",
@@ -23,8 +23,8 @@ export const products = [
     price: 30.00,
     images: ["/ultra-ball-1.jpg", "/ultra-ball-2.jpg", "/ultra-ball-3.jpg"],
     thumbnail: "/ultra-ball-png.png",
-    thumbnailOffsetY: -20,
-    thumbnailScale: 1.3,
+    thumbnailOffsetY: -100,
+    thumbnailScale: 1.5,
     description: "Inspirado en la Ultra Ball: acabado negro con banda amarilla y botón central blanco. Misma resistencia y precisión de encastre que el resto de la línea premium.",
     specs: {
       medidas: "Exteriores: 10.5 cm x 8.2 cm | Interiores (para la carta): 9.2 cm x 6.8 cm",
@@ -40,8 +40,8 @@ export const products = [
     price: 30.00,
     images: ["/premier-ball-1.jpg", "/premier-ball-2.jpg", "/premier-ball-3.jpg"],
     thumbnail: "/premier-ball-png.png",
-    thumbnailOffsetY: -10,
-    thumbnailScale: 1.05,
+    thumbnailOffsetY: -80,
+    thumbnailScale: 1.5,
     description: "Inspirado en la Premier Ball: acabado blanco con franja roja central y botón blanco. Elegante y minimalista para tus cartas más valiosas.",
     specs: {
       medidas: "Exteriores: 10.5 cm x 8.2 cm | Interiores (para la carta): 9.2 cm x 6.8 cm",
@@ -58,7 +58,7 @@ export const products = [
     images: ["/pokebola-1.jpg", "/pokebola-2.jpg", "/pokebola-3.jpg"],
     thumbnail: "/pokebola-png.png",
     thumbnailOffsetY: -15,
-    thumbnailScale: 1.2,
+    thumbnailScale: 1.5,
     description: "Inspirado en el diseño icónico de la Poké Ball original: rojo arriba, blanco abajo, con línea negra divisoria. El clásico que nunca falla.",
     specs: {
       medidas: "Exteriores: 10.5 cm x 8.2 cm | Interiores (para la carta): 9.2 cm x 6.8 cm",
@@ -74,7 +74,7 @@ export const products = [
     price: 30.00, // TODO: Christopher va a actualizar este precio
     images: ["/dispensador-1.jpg", "/dispensador-2.jpg", "/dispensador-3.jpg"],
     thumbnail: "/dispensador-png.png",
-    thumbnailOffsetY: 0,
+    thumbnailOffsetY: -20,
     thumbnailScale: 1.0,
     description: "Organiza y protege tu stock de toploaders. Diseñado para guardar varias unidades apiladas, fácil de recargar y con acabado resistente a golpes.",
     specs: {

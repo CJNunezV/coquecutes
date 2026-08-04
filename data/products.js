d125f11e03ce8aecdbea03eaec1b5472687c8355
christopher-airc <christopher@airc.digital>
2026-08-04 11:25:16 -0500

    a

--- data/products.js
+++ data/products.js
@@ -6,7 +6,7 @@ export const products = [
     price: 30.00,
     images: ["/team-rocket-1.jpg", "/team-rocket-2.jpg", "/team-rocket-3.jpg"],
     thumbnail: "/team-rocket-png.png",
-    thumbnailOffsetY: 150, // px: negativo sube la imagen, positivo la baja
+    thumbnailOffsetY: 20, // px: negativo sube la imagen, positivo la baja
     thumbnailScale: 1.5,
     description: "Protector premium diseñado especialmente para cartas coleccionables de formato estándar. Acabado negro y rojo con el icónico logo \"R\" del Team Rocket, para tus cartas con más actitud.",
     specs: {
@@ -23,7 +23,7 @@ export const products = [
     price: 30.00,
     images: ["/ultra-ball-1.jpg", "/ultra-ball-2.jpg", "/ultra-ball-3.jpg"],
     thumbnail: "/ultra-ball-png.png",
-    thumbnailOffsetY: 180,
+    thumbnailOffsetY: 30,
     thumbnailScale: 1.5,
     description: "Inspirado en la Ultra Ball: acabado negro con banda amarilla y botón central blanco. Misma resistencia y precisión de encastre que el resto de la línea premium.",
     specs: {
@@ -40,7 +40,7 @@ export const products = [
     price: 30.00,
     images: ["/premier-ball-1.jpg", "/premier-ball-2.jpg", "/premier-ball-3.jpg"],
     thumbnail: "/premier-ball-png.png",
-    thumbnailOffsetY: 160,
+    thumbnailOffsetY: 40,
     thumbnailScale: 1.5,
     description: "Inspirado en la Premier Ball: acabado blanco con franja roja central y botón blanco. Elegante y minimalista para tus cartas más valiosas.",
     specs: {
@@ -57,7 +57,7 @@ export const products = [
     price: 30.00,
     images: ["/pokebola-1.jpg", "/pokebola-2.jpg", "/pokebola-3.jpg"],
     thumbnail: "/pokebola-png.png",
-    thumbnailOffsetY: 150,
+    thumbnailOffsetY: 50,
     thumbnailScale: 1.5,
     description: "Inspirado en el diseño icónico de la Poké Ball original: rojo arriba, blanco abajo, con línea negra divisoria. El clásico que nunca falla.",
     specs: {
